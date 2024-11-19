13f08d2bcf26c4326f8ee434967bd24c96efee6b
Sharon <masseaudrey@yahoo.fr>
2024-11-19 18:00:49 +0100

    Commit 1

---
+++ ML/main.py
@@ -0,0 +1,7 @@
+import os
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.svm import SVC
+from sklearn.preprocessing import LabelEncoder
